9bd7a222ba07ce4f3102a82569e225b9d7a2b021
srinath-3898 <srinath3898@gmail.com>
2023-09-17 09:04:36 +0530

    signin completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,19 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const signup = async (req, res) => {
   try {
-    const { fullName, email, mobile, password, confirm_password } = req.body;
-    if (!fullName || !email || !mobile || !password || !confirm_password) {
+    const { fullName, email, mobile, password, confirmPassword } = req.body;
+    if (!fullName || !email || !mobile || !password || !confirmPassword) {
       return res.status(400).json({
         status: false,
         data: null,
         message: "Missing required fields",
       });
     }
-    if (password !== confirm_password) {
+    if (password !== confirmPassword) {
       return res.status(400).json({
         status: false,
         data: null,
@@ -46,9 +48,52 @@ const signup = async (req, res) => {
       message: "Signedup successfully, please signin to continue...",
     });
   } catch (error) {
+    console.log(error);
     return res
       .status(500)
       .json({ status: false, data: null, message: error.message });
   }
 };
-module.exports = { signup };
+
+const generateToken = ({ id, fullName, email, mobile }) =>
+  jwt.sign({ id, fullName, email, mobile }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        status: false,
+        data: null,
+        message: "Missing email or password",
+      });
+    }
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: false, data: null, message: "User not found" });
+    }
+    const comparedPassword = await bcrypt.compare(password, user.password);
+    if (user && !comparedPassword) {
+      return res
+        .status(403)
+        .json({ status: false, data: null, message: "Invalid credentials" });
+    }
+    const token = generateToken(user);
+    return res.status(201).json({
+      status: true,
+      data: { token },
+      message: "Logged in successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: false, data: null, message: error.message });
+  }
+};
+
+module.exports = { signup, signin };
